0f45586b779c0de686801af1f0c68ff72a8e5f19
xiexuanzheng123 <349328984@qq.com>
2018-02-02 13:56:36 +0800

    init and it works

---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    devtool: 'cheap-eval-module-source',
+    entry: {
+        index: [
+            'babel-polyfill',
+            './index.js'
+        ]
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        publicPath: '/dist/',
+        filename: '[name].bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\js$/,
+                exclude: /(node_modules|browser_components)/,
+                use: [
+                    {
+                        loader: 'babel-loader'
+                    }
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                    }
+                ]
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: 'images/[name].[ext]'
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NamedChunksPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'TypeScript',
+            hash: true,
+            chunks: ['index'],
+            template: './index.html',
+            favicon: './src/images/ts.png'
+        })
+    ],
+    resolve: {
+        extensions: ['.js', '.json']
+    }
+}
